77851970ed0810c81c311181052e4029603bb18a
cocolopez <Andreslondero@gmail.com>
2016-05-19 17:48:59 -0300

    Slim changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_n7100-user
-add_lunch_combo aosp_n7100-userdebug
-add_lunch_combo aosp_n7100-eng
+add_lunch_combo slim_n7100-user
+add_lunch_combo slim_n7100-userdebug
+add_lunch_combo slim_n7100-eng
